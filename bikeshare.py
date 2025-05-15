a8ecfc2df0a8683e49eeb92f3119e6b8f6ef11b8
Pratiti-Soumya <pratitisoumya11@outlook.com>
2025-05-14 22:25:14 -0700

    Added comments related to loading data in city_data dataframe

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+#Import and load data into City Data Dataframe"
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
